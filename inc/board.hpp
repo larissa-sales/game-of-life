ce2e0b57f11bbf97d596ddaa51382efa1771e4d2
Larissa Sales <larissa.s.sales@outlook.com>
2017-09-18 08:04:51 -0300

    implementadas função e variável para checar células vizinhas

--- inc/board.hpp
+++ inc/board.hpp
@@ -7,13 +7,15 @@
 using namespace std;
 
 #define LINE 20
-#define COL 36
+#define COL 38
 
 typedef Cell(&MatrixCell)[LINE][COL];
 
 class Board{
 private:
 	Cell field[LINE][COL];	
+	int neighbors[LINE][COL];
+
 public:
 	Board();
 	~Board();
@@ -22,6 +24,10 @@ public:
 	void setField(MatrixCell field);
 
 	void printBoard();
+
+	void checkNeighbors();
+
+	void checkRules();
 };
 
 #endif
--- src/board.cpp
+++ src/board.cpp
@@ -24,6 +24,9 @@ void Board::setField(MatrixCell field){
 
 void Board::printBoard(){
 	int l, c;
+	for(l=0; l<20; l++){
+		cout << endl;
+	}
 
 	for (l=0; l<LINE; l++){
 		for (c=0; c<COL; c++){
@@ -31,9 +34,83 @@ void Board::printBoard(){
 				cout << "o ";
 			} 
 			else{
-				cout << "  ";
+				cout << "- ";
 			}
 		}
 		cout << endl;
+	}
+}
+
+void Board::checkNeighbors(){
+	int l, c, counter;
+
+	for (l=0; l<LINE; l++){
+		for (c=0; c<COL; c++){
+			counter = 0;
+
+			if ((l-1)>=0 && (c-1)>=0){
+				if(field[l-1][c-1].getAlive()){
+					counter++;
+				}
+			} 
+			if ((l-1)>=0 && (c)>=0){
+				if(field[l-1][c].getAlive()){
+					counter++;
+				}
+			}
+			if ((l-1)>=0 && (c+1)<COL){
+				if(field[l-1][c+1].getAlive()){
+					counter++;
+				}
+			}
+			if ((l)>=0 && (c-1)>=0){
+				if(field[l][c-1].getAlive()){
+					counter++;
+				}
+			}
+			if ((l)>=0 && (c+1)<COL){
+				if(field[l][c+1].getAlive()){
+					counter++;
+				}
+			}
+			if ((l+1)<LINE && (c-1)>=0){
+				if(field[l+1][c-1].getAlive()){
+					counter++;
+				}
+			}
+			if ((l+1)<LINE && (c)>=0){
+				if(field[l+1][c].getAlive()){
+					counter++;
+				}
+			}
+			if ((l+1)<LINE && (c+1)<COL){
+				if(field[l+1][c+1].getAlive()){
+					counter++;
+				}
+			}
+			neighbors[l][c] = counter;
+		}
+	}
+}
+
+void Board::checkRules(){
+	int l, c;
+
+	checkNeighbors();
+
+	for (l=0; l<LINE; l++){
+		for (c=0; c<COL; c++){
+			if (field[l][c].getAlive()==true){
+				if (neighbors[l][c]<2){
+					field[l][c].setAlive(false);
+				} else if (neighbors[l][c]>3){
+					field[l][c].setAlive(false);
+				}
+			} else{
+				if (neighbors[l][c] == 3){
+					field[l][c].setAlive(true);
+				}
+			}
+		}
 	}
 }
