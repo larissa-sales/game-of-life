a519dc2256aebfcf3103d52a3224a5d70f4b8fed
Larissa Sales <larissa.s.sales@outlook.com>
2017-09-18 07:57:22 -0300

    corrigido bug do block na posição errada

--- src/gosper.cpp
+++ src/gosper.cpp
@@ -3,45 +3,45 @@
 MatrixCell Gosper::setGosper(MatrixCell field, int x, int y){
 	setField(field);
 
-	getField()[4][0].setAlive(true);
-	getField()[5][0].setAlive(true);
-	getField()[4][1].setAlive(true);
-	getField()[5][1].setAlive(true);
+	getField()[5][2].setAlive(true);
+	getField()[6][2].setAlive(true);
+	getField()[5][3].setAlive(true);
+	getField()[6][3].setAlive(true);
 
-	getField()[4][11].setAlive(true);
-	getField()[5][11].setAlive(true);
-	getField()[6][11].setAlive(true);
-	getField()[3][12].setAlive(true);
+	getField()[5][12].setAlive(true);
+	getField()[6][12].setAlive(true);
 	getField()[7][12].setAlive(true);
-	getField()[2][13].setAlive(true);
+	getField()[4][13].setAlive(true);
 	getField()[8][13].setAlive(true);
-	getField()[2][14].setAlive(true);
-	getField()[8][14].setAlive(true);
-	getField()[5][15].setAlive(true);
-	getField()[3][16].setAlive(true);
-	getField()[7][16].setAlive(true);
+	getField()[3][14].setAlive(true);
+	getField()[9][14].setAlive(true);
+	getField()[3][15].setAlive(true);
+	getField()[9][15].setAlive(true);
+	getField()[6][16].setAlive(true);
 	getField()[4][17].setAlive(true);
-	getField()[5][17].setAlive(true);
-	getField()[6][17].setAlive(true);
+	getField()[8][17].setAlive(true);
 	getField()[5][18].setAlive(true);
+	getField()[6][18].setAlive(true);
+	getField()[7][18].setAlive(true);
+	getField()[6][19].setAlive(true);
 
-	getField()[2][21].setAlive(true);
-	getField()[3][21].setAlive(true);
-	getField()[4][21].setAlive(true);
-	getField()[2][22].setAlive(true);
 	getField()[3][22].setAlive(true);
 	getField()[4][22].setAlive(true);
-	getField()[1][23].setAlive(true);
+	getField()[5][22].setAlive(true);
+	getField()[3][23].setAlive(true);
+	getField()[4][23].setAlive(true);
 	getField()[5][23].setAlive(true);
-	getField()[0][25].setAlive(true);
-	getField()[1][25].setAlive(true);
-	getField()[5][25].setAlive(true);
-	getField()[6][25].setAlive(true);
+	getField()[2][24].setAlive(true);
+	getField()[6][24].setAlive(true);
+	getField()[1][26].setAlive(true);
+	getField()[2][26].setAlive(true);
+	getField()[6][26].setAlive(true);
+	getField()[7][26].setAlive(true);
 
-	getField()[3][34].setAlive(true);
-	getField()[4][34].setAlive(true);
-	getField()[3][35].setAlive(true);
-	getField()[4][35].setAlive(true);
+	getField()[4][36].setAlive(true);
+	getField()[5][36].setAlive(true);
+	getField()[4][37].setAlive(true);
+	getField()[5][37].setAlive(true);
 
 	return getField();
 }
